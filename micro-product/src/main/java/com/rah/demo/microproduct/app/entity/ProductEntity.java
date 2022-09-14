06b5a285f36b1109e5fd3bd91f18116afdade0a6
robinarehen <robinarehen@gmail.com>
2022-09-14 17:27:25 +0200

    refactor: 1. se agrega el proyecto micro-product

---
+++ micro-product/src/main/java/com/rah/demo/microproduct/app/entity/ProductEntity.java
@@ -0,0 +1,19 @@
+package com.rah.demo.microproduct.app.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "products")
+public class ProductEntity {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+}
